dc4de6074830bbe2cf6e85fd71ebfceb5c99206a
zkp3 <zkp3@tutamail.com>
2025-07-07 22:25:27 +0700

    Add files via upload

--- data/langs/en.py
+++ data/langs/en.py
@@ -1,4 +1,4 @@
-def getDialog(index, bgs, fonts):
+def getTexts(bgs, fonts, getDlogs):
     dialogs = {
         -1: 0,
         0:{
@@ -7,8 +7,11 @@ def getDialog(index, bgs, fonts):
             'size': 35,
             'color': (255, 255, 255),
             'bg': bgs['darkOvrly'],
-            'font': fonts['DFfontPath'],
             'pos': [0, 0],
+            'fontPath': fonts['DFfontPath'],
+            'delay': 0,
+            'letterIndex': 0,
+            'speed': 3
         },
         1:{
             'text': 'It continues to stand actively in the same place?',
@@ -16,7 +19,10 @@ def getDialog(index, bgs, fonts):
             'color': (255, 255, 0),
             'bg': bgs['darkOvrly'],
             'pos': [0, 200],
-            'font': fonts['ComicFontPath'],
+            'fontPath': fonts['ComicFontPath'],
+            'delay': 0,
+            'letterIndex': 0,
+            'speed': 3
         },
         2:{
             'text': ':)',
@@ -24,7 +30,31 @@ def getDialog(index, bgs, fonts):
             'color': (0, 255, 255),
             'bg': bgs['darkOvrly'],
             'pos': [0, 400],
-            'font': fonts['WingdingFontPath'],
+            'fontPath': fonts['WingdingFontPath'],
+            'delay': 0,
+            'letterIndex': 0,
+            'speed': 3
         }
     }
-    return dialogs[index]
+    texts = {
+        -1: 0,
+        0:  {
+            'fontPath': fonts['DFfontPath'],
+            'size': 30,
+            'text': 'EXIT? (ESC+Q)',
+            'color': (200, 200, 150),
+            'pos': [0, 0],
+            'center': 0
+        },
+        1:  {
+            'fontPath': fonts['DFfontPath'],
+            'size': 50,
+            'text': '/!GAME MENU!\\',
+            'color': (255, 0, 0),
+            'pos': [400, 300],
+            'center': 1
+        }
+    }
+    if getDlogs:
+        return dialogs
+    else: return texts
--- data/langs/ru.py
+++ data/langs/ru.py
@@ -1,4 +1,4 @@
-def getDialog(index, bgs, fonts):
+def getTexts(bgs, fonts, getDlogs):
     dialogs = {
         -1: 0,
         0:{
@@ -7,8 +7,11 @@ def getDialog(index, bgs, fonts):
             'size': 35,
             'color': (255, 255, 255),
             'bg': bgs['darkOvrly'],
-            'font': fonts['DFfontPath'],
             'pos': [0, 0],
+            'fontPath': fonts['DFfontPath'],
+            'delay': 0,
+            'letterIndex': 0,
+            'speed': 3
         },
         1:{
             'text': 'Он продолжает активно стоять на том же месте?',
@@ -16,7 +19,10 @@ def getDialog(index, bgs, fonts):
             'color': (255, 255, 0),
             'bg': bgs['darkOvrly'],
             'pos': [0, 200],
-            'font': fonts['ComicFontPath'],
+            'fontPath': fonts['ComicFontPath'],
+            'delay': 0,
+            'letterIndex': 0,
+            'speed': 3
         },
         2:{
             'text': ':)',
@@ -24,7 +30,31 @@ def getDialog(index, bgs, fonts):
             'color': (0, 255, 255),
             'bg': bgs['darkOvrly'],
             'pos': [0, 400],
-            'font': fonts['WingdingFontPath'],
+            'fontPath': fonts['WingdingFontPath'],
+            'delay': 0,
+            'letterIndex': 0,
+            'speed': 3
         }
     }
-    return dialogs[index]
+    texts = {
+        -1: 0,
+        0:  {
+            'fontPath': fonts['DFfontPath'],
+            'size': 30,
+            'text': 'ВЫХОД? (ESC+Q)',
+            'color': (200, 200, 150),
+            'pos': [0, 0],
+            'center': 0
+        },
+        1:  {
+            'fontPath': fonts['DFfontPath'],
+            'size': 50,
+            'text': '/!ИГРОВОЕ МЕНЮ!\\',
+            'color': (255, 0, 0),
+            'pos': [400, 300],
+            'center': 1
+        }
+    }
+    if getDlogs:
+        return dialogs
+    else: return texts
